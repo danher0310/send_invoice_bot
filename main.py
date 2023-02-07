7c8bd4100ba92039c3adfb846507286ecdad7850
Daniel <daniel.herrera2292@gmail.com>
2023-02-07 18:13:37 -0400

    Working on funtion to send the invice

--- main.py
+++ main.py
@@ -1,7 +1,69 @@
-from dotenv import load_dotenv, dotenv_values
-import os
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import logging
+from datetime import datetime, date, time
+import calendar
+import pytz
+from dotenv import load_dotenv
+import os 
+import utils
 
-dotenv_values()
+load_dotenv()
 
-config = dotenv_values('.env')
-print(config)
+now = datetime.now()
+dates = now.strftime("%m/%d/%Y")
+invoice_date = now.strftime("%m-%d-%Y")
+month = now.month
+year =  now.year
+day = now.day 
+last_day = calendar.monthrange (year,month) [1]  
+  
+
+if day <= 10:
+  firstDay = date(year, month, 1).strftime("%m/%d/%Y")
+  lastDay = date(year,month, 15).strftime("%m/%d/%Y")
+
+elif day >= 11:
+  firstDay = date(year, month, 16).strftime("%m/%d/%Y")
+  lastDay = date(year,month, last_day).strftime("%m/%d/%Y")
+
+async def start(update, context):
+  user = update.effective_user
+  await update.message.reply_text(f"Greetings {user.username} !")
+
+async def chatId(update,context):
+    """Returns the chat id where the bot responds to"""
+    chatId = update.message.chat.id
+    await update.message.reply_text(chatId)
+    
+async def send_invoice(update,context):
+  chatId = os.getenv('chat_id')
+  document = utils.create_invoice()
+  if os.path.isfile(document):
+    await context.bot.send_document(
+      chat_id = chatId,
+      document = document,
+      caption = f"Consulting services from {firstDay} to {lastDay}"
+    )
+  else:
+    await context.bot.send_message(
+      chat_id = chatId,
+      text = document
+    )
+    
+
+def main():
+  token = os.getenv('tlgtoken')
+  application = Application.builder().token(token).build()
+  logging.basicConfig(format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+  application.add_handler(CommandHandler('start', start))
+  application.add_handler(CommandHandler('chat_id', chatId))
+  application.add_handler(CommandHandler('send_invoice', send_invoice))
+  application.job_queue.run_monthly(send_invoice, time(hour=6, minute=50, second=0, tzinfo=pytz.timezone('US/Eastern')), 10)
+  print("-----Bot Started-----")
+  application.run_polling()
+    
+if __name__ == "__main__":
+  main()
+
+  
+  
--- utils.py
+++ utils.py
@@ -2,20 +2,63 @@ import jinja2
 import pdfkit
 from datetime import datetime, date
 import calendar
+import mysql.connector
 import os 
+from dotenv import load_dotenv, dotenv_values
 
-# def connectionDb():
+
+load_dotenv()
+def connectionDb():
+  try:
+    mydb=mysql.connector.connect(
+      host = os.getenv('dbhost'),
+      user = os.getenv('userdb'),
+      password = os.getenv('passdb'),
+      database = os.getenv('dbname'),
+      auth_plugin = "mysql_native_password"     
+    )
+    return mydb
+  except OSError:
+    return OSError
+  
+def check_and_get_number():
+  mydb = connectionDb()
+  myCursor = mydb.cursor()
+  myCursor.execute("SELECT invoicenumber from invoice_number order by invoicenumber DESC")
+  result = myCursor.fetchone()
+  mydb.close()
+  return result[0]+1
+  
+  
+def insert_invoice_number(invoiceNumber): 
+  try:
+    mydb = connectionDb()
+    myCursor = mydb.cursor()
+    scriptSQL = ("INSERT INTO invoice_number (invoicenumber) VALUES(%s)")
+    value = invoiceNumber
+    myCursor.execute(scriptSQL,(value,))
+    mydb.commit()
+    mydb.close()
+    return True
+  except OSError:
+    return False
+    
+  
+  
+  
   
 
 
 def create_invoice():
   now = datetime.now()
   dates = now.strftime("%m/%d/%Y")
-
+  invoice_date = now.strftime("%m-%d-%Y")
   month = now.month
   year =  now.year
   day = now.day 
   last_day = calendar.monthrange (year,month) [1] 
+  
+  
 
   if day <= 10:
     firstDay = date(year, month, 1).strftime("%m/%d/%Y")
@@ -28,28 +71,38 @@ def create_invoice():
   else: 
     firstDay = date(year, month, 1).strftime("%m/%d/%Y")
     lastDay = date(year,month, last_day).strftime("%m/%d/%Y")
-    
+  
+  invoiceN = check_and_get_number()
     
   context = {
   
     'dates':dates,
-    'invoiceN': '4040Test',
+    'invoiceN': invoiceN,
     'firstDay': firstDay,
     'lastDay': lastDay
   }
   template_loader = jinja2.FileSystemLoader(r'./template')
   template_env = jinja2.Environment(loader=template_loader )
+  path_saving = f"{os.getenv('path_saving')}{os.sep}Invoice_{invoice_date}.pdf"  
+  if not os.path.exists(path_saving):
+    template = template_env.get_template("invoice.html")
+    output_text = template.render(context)
 
-  template = template_env.get_template("invoice.html")
-  output_text = template.render(context)
+    config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe" )
+    if pdfkit.from_string(output_text, path_saving, configuration=config , css=r'./template/assets/css/index.css', options={"enable-local-file-access": ""} ):
+      if insert_invoice_number(int(invoiceN)):
+        return path_saving
+      else:
+        return "We have a error with the invoice"
+  else:
+    return "I created the invoice today"
+    
+    
+  
 
-  config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe" )
-  pdfkit.from_string(output_text, r'./pdf_test1.pdf', configuration=config , css=r'./template/assets/css/index.css', options={"enable-local-file-access": ""} )
 
-create_invoice()
 
-# list = os.listdir(r"C:\Program Files\wkhtmltopdf\bin")
-# print(list)
+
 
 
 
